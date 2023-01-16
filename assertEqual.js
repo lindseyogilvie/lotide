d634b08e8f3da4799ba6b92af2f58203c063ae13
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-16 12:29:03 -0800

    Refactored assertEqual and head files to be DRY

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Day 1", "Day 1");
-assertEqual(13, 31);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // FUNCTION IMPLEMENTATION. Create a function that returns the first item in an array.
 const head = function(array) {
   return array[0];
 }
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["apple"]), "apple");
-assertEqual(head([]), 7);
+module.exports = head;
