ddd679a0041b6563b67c910b5cfc5f8d30752c38
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-16 16:37:47 -0800

    Refactored middle and middleTest to make DRY by modularizing the code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,17 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual')
 
-// TEST CODE
-assertArraysEqual(middle([9]), []) // => Assertion Passed
-assertArraysEqual(middle([12, 13]), []) // => Assertion Passed
-
-assertArraysEqual(middle(['a', 'b', 'c']), ['b']) // => Assertion Passed
-assertArraysEqual(middle(['apple', 'bobcat', 'china', 'dogs', 'engaged']), ['china']) // => Assertion Passed
-
-assertArraysEqual(middle([4, 3, 2, 1]), [3, 2]) // => Assertion Passed
-assertArraysEqual(middle([6, 5, 4, 3, 2, 1]), [4, 3]) // => Assertion Passed
+describe("#middle", () => {
+  it("returns ['b'] from ['a', 'b', 'c']", () => {
+    assert.deepEqual(middle(['a', 'b', 'c']), ['b']);
+  });
+  it("returns ['b', 'c'] from ['a', 'b', 'c', 'd']", () => {
+    assert.deepEqual(middle(['a', 'b', 'c', 'd']), ['b', 'c']);
+  });
+  it("returns an empty array when passed an array with a single element", () => {
+    assert.deepEqual(middle([1]), []);
+  })
+  it("returns an empty array when passed an array with two elements", () => {
+    assert.deepEqual(middle(['a', 'b']), []);
+  })
+});
