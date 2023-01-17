965cbc484b393554d48803ddf9c031eb11cde5ea
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-16 16:39:20 -0800

    Refactored eqArrays and eqArraysTest to make DRY by modularizing the code

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,14 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => Assertion Passed
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => Assertion Passed
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false) // => Assertion Failed
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => Assertion Passed
+describe("#eqArrays", () => {
+  it("returns true for ['1', '2', '3'] and ['1', '2', '3']", () => {
+    assert.isTrue(eqArrays(['1', '2', '3'], ['1', '2', '3']));
+  });
+  it("returns false for ['1', '2', '3'] and ['3', '2', '1']", () => {
+    assert.isFalse(eqArrays(['1', '2', '3'], ['3', '2', '1']));
+  });
+  it("returns false for ['1', '2', '3'] and ['1', '2']", () => {
+    assert.isFalse(eqArrays(['1', '2', '3'], ['1', '2']));
+  });
+})
