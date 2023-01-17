e22ca55bab718014242dc299f794eab8ddc44fd7
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-16 16:32:25 -0800

    Fixed to return empty array when passed an empty array

--- head.js
+++ head.js
@@ -1,6 +1,10 @@
 // FUNCTION IMPLEMENTATION. Create a function that returns the first item in an array.
 const head = function(array) {
-  return array[0];
-}
+  if (array.length === 0) {
+    return [];
+  } else {
+    return array[0];
+  }
+};
 
 module.exports = head;
